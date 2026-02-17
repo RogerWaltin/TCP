6f1497493b17ecdf85644e0135f8ef5f56bc9986
Harris <harriswaltinjoy@gmail.com>
2026-02-17 18:29:58 +0530

    completed footer

--- src/footer.jsx
+++ src/footer.jsx
@@ -5,45 +5,77 @@ export function Footer() {
 
     return (
         <div>
-            <div>
-                <img src="https://wallpapers.com/images/hd/epic-battle-nxkj0vpvfmne5ykg.jpg" />
-            </div>
             <footer>
                 <hr className="my-4 mx-0 text-gray-200" />
                 <div id="listdiv" className="mx-35 my-6 flex gap-7 py-10">
-                    <ul className="list border-r pr-7 border-gray-300">
+                    <ul className="list border-r pr-7 border-gray-300 whitespace-nowrap">
                         <li className="font-bold text-xl">Pages</li>
                         <hr className="mt-2 w-10 border-2 text-[#9c3466]" />
-                        <li className="pt-5">Home</li>
-                        <li className="pt-5">About Us</li>
-                        <li className="pt-5">Privacy Policy</li>
-                        <li className="pt-5">Refund & Cancellation Policy</li>
-                        <li className="pt-5">Shipping & Delivery Policy</li>
-                        <li className="pt-5">Terms & Conditions</li>
+                        <li className="pt-5">
+                            <a href="#" className="hover:text-[#9c3466]">Home</a>
+                        </li>
+                        <li className="pt-5">
+                            <a href="#" className="hover:text-[#9c3466]">About Us</a>
+                        </li>
+                        <li className="pt-5">
+                            <a href="#" className="hover:text-[#9c3466]">Privacy Policy</a>
+                        </li>
+                        <li className="pt-5">
+                            <a href="#" className="hover:text-[#9c3466]">Refund & Cancellation Policy</a>
+                        </li>
+                        <li className="pt-5">
+                            <a href="#" className="hover:text-[#9c3466]">Shipping & Delivery Policy</a>
+                        </li>
+                        <li className="pt-5">
+                            <a href="#" className="hover:text-[#9c3466]">Terms & Conditions</a>
+                        </li>
                     </ul>
-                    <ul className="list border-r pr-17 border-gray-300">
+                    <ul className="list border-r pr-17 border-gray-300 whitespace-nowrap">
                         <li className="font-bold text-xl">Other Links</li>
                         <hr className="mt-2 w-10 border-2 text-[#9c3466]" />
-                        <li className="pt-5">Redemption Hill Church</li>
-                        <li className="pt-5">Mike's Personal Blog</li>
-                        <li className="pt-5">TCP YouTube Channel</li>
-                        <li className="pt-5">For The Truth</li>
-                        <li className="pt-5">Sola Books</li>
-                        <li className="pt-5">Equip Indian Churches</li>
+                        <li className="pt-5">
+                            <a href="#" className="hover:text-[#9c3466]">Redemption Hill Church</a>
+                        </li>
+                        <li className="pt-5">
+                            <a href="#" className="hover:text-[#9c3466]">Mike's Personal Blog</a>
+                        </li>
+                        <li className="pt-5">
+                            <a href="#" className="hover:text-[#9c3466]">TCP YouTube Channel</a>
+                        </li>
+                        <li className="pt-5">
+                            <a href="#" className="hover:text-[#9c3466]">For The Truth</a>
+                        </li>
+                        <li className="pt-5">
+                            <a href="#" className="hover:text-[#9c3466]">Sola Books</a>
+                        </li>
+                        <li className="pt-5">
+                            <a href="#" className="hover:text-[#9c3466]">Equip Indian Churches</a>
+                        </li>
                     </ul>
-                    <ul className="list border-r pr-12 border-gray-300">
+                    <ul className="list border-r pr-21 border-gray-300 whitespace-nowrap">
                         <li className="font-bold text-xl">Authors</li>
                         <hr className="mt-2 w-10 border-2 text-[#9c3466]" />
-                        <li className="pt-5">Michael Teddy</li>
-                        <li className="pt-5">Benjamin Abraham</li>
-                        <li className="pt-5">Ashok Mohanakumar</li>
-                        <li className="pt-5">Steve Waltin</li>
+                        <li className="pt-5">
+                            <a href="#" className="hover:text-[#9c3466]">Michael Teddy</a>
+                        </li>
+                        <li className="pt-5">
+                            <a href="#" className="hover:text-[#9c3466]">Benjamin Abraham</a>
+                        </li>
+                        <li className="pt-5">
+                            <a href="#" className="hover:text-[#9c3466]">Ashok Mohanakumar</a>
+                        </li>
+                        <li className="pt-5">
+                            <a href="#" className="hover:text-[#9c3466]">Steve Waltin</a>
+                        </li>
                     </ul>
-                    <ul className="list pl-12">
+                    <ul className="list ">
                         <li className="pt-5"><a href="https://thecrosspurpose.com"><img src="https://thecrosspurpose.com/wp-content/uploads/2022/09/logo-1.png" alt="logo" className="w-lg" /></a></li>
                         <li className="pt-5">Informing Minds. Reforming Hearts.</li>
-                        <ul className="flex">
-                            <li><img src="" alt="" /></li>
+                        <ul className="flex gap-6 mt-12 select-none"> {/*remember to import icons instead later*/}
+                            <li className="group rounded-full border-2 p-2 border-[#9c3466] hover:bg-[#9c3466] transition duration-400"><a href="#"><img className="group-hover:invert filter transition" width="26" height="26" src="https://img.icons8.com/metro/26/facebook-new--v2.png" alt="facebook-new--v2" /></a></li>
+                            <li className="group rounded-full border-2 p-2 border-[#9c3466] hover:bg-[#9c3466] transition duration-400"><a href="#"><img className="group-hover:invert filter transition" width="26" height="26" src="https://img.icons8.com/metro/26/instagram-new.png" alt="instagram-new" /></a></li>
+                            <li className="group rounded-full border-2 p-2 border-[#9c3466] hover:bg-[#9c3466] transition duration-400"><a href="#"><img className="group-hover:invert filter transition" width="26" height="26" src="https://img.icons8.com/glyph-neue/64/youtube-play.png" alt="youtube-play" /></a></li>
+                            <li className="group rounded-full border-2 p-2 border-[#9c3466] hover:bg-[#9c3466] transition duration-400"><a href="#"><img className="group-hover:invert filter transition" width="26" height="26" src="https://img.icons8.com/ios-glyphs/30/twitterx--v2.png" alt="twitterx--v2" /></a></li>
                         </ul>
                     </ul>
                 </div>
