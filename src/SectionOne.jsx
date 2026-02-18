2f83cbab67fc86114a8d0872bc8f95a88063d6f4
Roger Waltin <Roger.mwaltin@gmail.com>
2026-02-18 17:40:03 +0530

    progressed

--- src/SectionOne.jsx
+++ src/SectionOne.jsx
@@ -4,7 +4,7 @@ function SectionOne() {
     const underlineEffect = "after:content-[''] after:w-0 after:h-0.5 after:bg-black after:absolute relative after:bottom-0 after:left-0 ml-10 hover:after:w-full after:transition-all after:duration-500 inline-block hover:cursor-pointer"
 
     return (
-        <div className="flex justify-center mt-10 bg-amber-50  ml-30">
+        <div className="flex  mt-10 bg-amber-50  ml-30">
             <div id="left-side" className="">
                 <img src={whatGospel} alt="what_gospel" className="w-2xs" />
                 <a href="" className={`${underlineEffect}`}>What is the Gospel?</a>
