cbcc408a05d78a782ba99def17db462069960e37
Harris <harriswaltinjoy@gmail.com>
2026-02-21 13:43:13 +0530

    created sect4

---
+++ src/Section4.jsx
@@ -0,0 +1,12 @@
+import { FaFacebook } from "react-icons/fa";
+import "./styles.css"
+
+export default function Section4() {
+
+
+    return (
+        <div className="mx-32">
+            
+        </div>
+    )
+}
